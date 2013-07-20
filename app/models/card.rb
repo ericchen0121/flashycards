ea5829ace041563220c7a8cd6d0caf4d651e1870
syn-splendidus <dewrgleision@gmail.com>
2013-07-20 16:25:34 -0700

    Added validates statements to the appopriate models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,6 @@
 class Card < ActiveRecord::Base
   has_many :guesses
   belongs_to :deck
+  validates :term, presence: true
+  validates :definition, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :rounds
+  validates :name, presence: true
+  validates :password, presence: true
 end
