a6317cd77959f54dd9484c2852bbac21eeee0cee
adamloo85 <adamloo85@gmail.com>
2013-07-19 17:38:37 -0700

    fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ Deck.create(name: "States and Capitals")
 
 state_deck = Deck.where(name: "States and Capitals").first.cards
 
-state_deck << (term: "California", definition: "Sacramento")
-state_deck << (term: "Hawaii", definition: "Honolulu")
-state_deck << (term: "Virginia", definition: "Richmond")
+state_deck << Card.create(term: "California", definition: "Sacramento")
+state_deck << Card.create(term: "Hawaii", definition: "Honolulu")
+state_deck << Card.create(term: "Virginia", definition: "Richmond")
