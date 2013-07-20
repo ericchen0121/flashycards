331e8bf229460bec1700dccdf89584c3e0facd28
adamloo85 <adamloo85@gmail.com>
2013-07-19 17:40:20 -0700

    fixed users

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -1,13 +1,11 @@
 post '/create' do
   User.create(name: params[:name], password: params[:password])
-  erb :login
 end
 
 post '/login' do
   @user = User.where(name: params[:name], password: params[:password]).first
 
   if @user != nil
-    erb :login
   else
     @message = "This user is not in the database.  Please make a new account!"
     erb :index
