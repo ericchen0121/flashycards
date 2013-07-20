785f1385c001569f3eb52a14b9b1031a405ea13e
syn-splendidus <dewrgleision@gmail.com>
2013-07-20 16:36:57 -0700

    Added support to detect user name based on downcase (case insensitive) and to prevent multiple users from having the same name

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -1,19 +1,27 @@
 enable :sessions
 
 post '/create' do
-  current_user = User.create(name: params[:name], password: params[:password])
-  session[:user_id] = current_user.id
-  redirect '/decks'
+  if params[:name] == "" || params[:password] == ""
+    @message = "User name and password are requires fields."
+    erb :index
+  elsif User.where(name: params[:name].downcase) == params[:name].downcase
+    @message = "This user already exists, please select a different user name."
+    erb :index
+  else
+    current_user = User.create(name: params[:name].downcase, password: params[:password])
+    session[:user_id] = current_user.id
+    redirect '/decks'
+  end
 end
 
 post '/login' do
-  current_user = User.where(name: params[:name], password: params[:password]).first
+  current_user = User.where(name: params[:name].downcase, password: params[:password]).first
 
   if current_user != nil
     session[:user_id] = current_user.id
     redirect '/decks'
   else
-    @message = "This user is not in the database.  Please make a new account!"
+    @message = "This user does not exist, please make a new account."
     erb :index
   end
 end
