4ae2caee9a50dc1f10d61b0924ab3433570f3c82
ericchen0121 <eric.chen0121@gmail.com>
2013-07-21 01:19:19 -0700

    added all state capitals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-state_deck = Deck.create(name: "States and Capitals").cards
-state_deck << Card.create(term: "Sacramento", definition: "California")
-state_deck << Card.create(term: "Honolulu", definition: "Hawaii")
-state_deck << Card.create(term: "Richmond", definition: "Virginia")
-state_deck << Card.create(term: "Salem", definition: "Oregon")
-state_deck << Card.create(term: "Providence", definition: "Rhode Island")
-state_deck << Card.create(term: "Olympia", definition: "Washington")
+state_example_deck = Deck.create(name: "States and Capitals - 6 cards").cards
+state_example_deck << Card.create(term: "Sacramento", definition: "California")
+state_example_deck << Card.create(term: "Honolulu", definition: "Hawaii")
+state_example_deck << Card.create(term: "Richmond", definition: "Virginia")
+state_example_deck << Card.create(term: "Salem", definition: "Oregon")
+state_example_deck << Card.create(term: "Providence", definition: "Rhode Island")
+state_example_deck << Card.create(term: "Olympia", definition: "Washington")
 
 cat_deck = Deck.create(name: "Famous Cats").cards
-cat_deck << Card.create(term: "Grumpy Cat", definition: "A foul-tempered feline")
-cat_deck << Card.create(term: "Ceiling Cat", definition: "Watches you from afar")
-cat_deck << Card.create(term: "Garfield", definition: "Loves Lasagna")
-cat_deck << Card.create(term: "Monorail Cat", definition: "Hovers in the air")
-cat_deck << Card.create(term: "Chesire Cat", definition: "Is magical and annoying and purple and grins like a sociopath")
-cat_deck << Card.create(term: "Mr. Bigglesworth", definition: "Belongs to Dr. Evil")
-cat_deck << Card.create(term: "Keyboard Cat", definition: "Is a keyboard master")
+cat_deck << Card.create(term: "Grumpy Cat", definition: "Internet sensation: A foul-tempered feline")
+cat_deck << Card.create(term: "Ceiling Cat", definition: "Internet lovable: Watches you from afar")
+cat_deck << Card.create(term: "Garfield", definition: "Cartoon conglomerate: Loves Lasagna")
+cat_deck << Card.create(term: "Monorail Cat", definition: "Youtube hog: Hovers in the air")
+cat_deck << Card.create(term: "Chesire Cat", definition: "Old timer: Is magical and annoying and purple and grins like a sociopath")
+cat_deck << Card.create(term: "Mr. Bigglesworth", definition: "Movie star: Belongs to Dr. Evil")
+cat_deck << Card.create(term: "Keyboard Cat", definition: "Viral hit: Is a keyboard master")
 
 test_deck_1 = Deck.create(name: "Test Deck One").cards
 test_deck_1 << Card.create(term: "one", definition: "1")
@@ -26,3 +26,55 @@ test_deck_2 << Card.create(term: "one", definition: "1")
 test_deck_2 << Card.create(term: "two", definition: "2")
 test_deck_2 << Card.create(term: "three", definition: "3")
 test_deck_2 << Card.create(term: "four", definition: "4")
+
+state_deck = Deck.create(name: "States and Capitals").cards
+state_deck << Card.create(term: "Alabama"  definition: "Montgomery")
+state_deck << Card.create(term: "Alaska"  definition: "Juneau")
+state_deck << Card.create(term: "Arizona"  definition: "Phoenix")
+state_deck << Card.create(term: "Arkansas"  definition: "Little Rock)"
+state_deck << Card.create(term: "California"  definition: "Sacramento")
+state_deck << Card.create(term: "Colorado"  definition: "Denver")
+state_deck << Card.create(term: "Connecticut" definition: "Hartford")
+state_deck << Card.create(term: "Delaware"  definition: "Dover")
+state_deck << Card.create(term: "Florida"  definition: "Tallahassee")
+state_deck << Card.create(term: "Georgia"  definition: "Atlanta")
+state_deck << Card.create(term: "Hawaii"  definition: "Honolulu")
+state_deck << Card.create(term: "Idaho"  definition: "Boise")
+state_deck << Card.create(term: "Illinois"  definition: "Springfield")
+state_deck << Card.create(term: "Indiana"  definition: "Indianapolis")
+state_deck << Card.create(term: "Iowa"  definition: "Des Moines)"
+state_deck << Card.create(term: "Kansas"  definition: "Topeka")
+state_deck << Card.create(term: "Kentucky"  definition: "Frankfort")
+state_deck << Card.create(term: "Louisiana"  definition: "Baton Rouge)"
+state_deck << Card.create(term: "Maine"  definition: "Augusta")
+state_deck << Card.create(term: "Maryland"  definition: "Annapolis")
+state_deck << Card.create(term: "Massachusetts"  definition: "Boston")
+state_deck << Card.create(term: "Michigan"  definition: "Lansing")
+state_deck << Card.create(term: "Minnesota"  definition: "St.) Paul"
+state_deck << Card.create(term: "Mississippi " definition: "Jackson")
+state_deck << Card.create(term: "Missouri"  definition: "Jefferson City)"
+state_deck << Card.create(term: "Montana"  definition: "Helena")
+state_deck << Card.create(term: "Nebraska"  definition: "Lincoln")
+state_deck << Card.create(term: "Nevada"  definition: "Carson City)"
+state_deck << Card.create(term: "New Hampshiredefinition: "  "Concord)"
+state_deck << Card.create(term: "New Jerseydefinition: "  "Trenton)"
+state_deck << Card.create(term: "New Mexicodefinition: "  "Santa) Fe"
+state_deck << Card.create(term: "New Yorkdefinition: "  "Albany)"
+state_deck << Card.create(term: "North Carolinadefinition: "  "Raleigh)"
+state_deck << Card.create(term: "North Dakotadefinition: "  "Bismarck)"
+state_deck << Card.create(term: "Ohio"  definition: "Columbus")
+state_deck << Card.create(term: "Oklahoma"  definition: "Oklahoma City)"
+state_deck << Card.create(term: "Oregon"  definition: "Salem")
+state_deck << Card.create(term: "Pennsylvania"  definition: "Harrisburg")
+state_deck << Card.create(term: "Rhode Islanddefinition: "  "Providence)"
+state_deck << Card.create(term: "South Carolinadefinition: "  "Columbia)"
+state_deck << Card.create(term: "South Dakotadefinition: "  "Pierre)"
+state_deck << Card.create(term: "Tennessee"  definition: "Nashville")
+state_deck << Card.create(term: "Texas"  definition: "Austin")
+state_deck << Card.create(term: "Utah"  definition: "Salt Lake) City"
+state_deck << Card.create(term: "Vermont"  definition: "Montpelier")
+state_deck << Card.create(term: "Virginia"  definition: "Richmond")
+state_deck << Card.create(term: "Washington"  definition: "Olympia")
+state_deck << Card.create(term: "West Virginiadefinition: "  "Charleston)"
+state_deck << Card.create(term: "Wisconsin"  definition: "Madison")
+state_deck << Card.create(term: "Wyoming"  definition: "Cheyenne")
