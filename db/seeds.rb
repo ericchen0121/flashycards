01595e72b8b3a04f71a09051af42e1c9650be1f4
jaredstander <jaredstander@gmail.com>
2013-07-20 21:44:49 -0700

    Users can delete cards and decks, as well as seeing all of the cards in a deck

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 state_deck = Deck.create(name: "States and Capitals").cards
-
 state_deck << Card.create(term: "Sacramento", definition: "California")
 state_deck << Card.create(term: "Honolulu", definition: "Hawaii")
 state_deck << Card.create(term: "Richmond", definition: "Virginia")
@@ -7,10 +6,7 @@ state_deck << Card.create(term: "Salem", definition: "Oregon")
 state_deck << Card.create(term: "Providence", definition: "Rhode Island")
 state_deck << Card.create(term: "Olympia", definition: "Washington")
 
-
-
 cat_deck = Deck.create(name: "Famous Cats").cards
-
 cat_deck << Card.create(term: "Grumpy Cat", definition: "A foul-tempered feline")
 cat_deck << Card.create(term: "Ceiling Cat", definition: "Watches you from afar")
 cat_deck << Card.create(term: "Garfield", definition: "Loves Lasagna")
@@ -19,8 +15,14 @@ cat_deck << Card.create(term: "Chesire Cat", definition: "Is magical and annoyin
 cat_deck << Card.create(term: "Mr. Bigglesworth", definition: "Belongs to Dr. Evil")
 cat_deck << Card.create(term: "Keyboard Cat", definition: "Is a keyboard master")
 
+test_deck_1 = Deck.create(name: "Test Deck One").cards
+test_deck_1 << Card.create(term: "one", definition: "1")
+test_deck_1 << Card.create(term: "two", definition: "2")
+test_deck_1 << Card.create(term: "three", definition: "3")
+test_deck_1 << Card.create(term: "four", definition: "4")
 
-
-
-
-  
+test_deck_2 = Deck.create(name: "Test Deck Two").cards
+test_deck_2 << Card.create(term: "one", definition: "1")
+test_deck_2 << Card.create(term: "two", definition: "2")
+test_deck_2 << Card.create(term: "three", definition: "3")
+test_deck_2 << Card.create(term: "four", definition: "4")
